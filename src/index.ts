cf494a0a7dc9302d1b86a7ca91c35cbfa1a36060
gabemaldito <gabrielraphacosta@google.com>
2026-05-20 19:25:56 +0200

    feat: update package.json and package-lock.json for new dependencies; refactor tsconfig.json and index.ts for improved structure

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
 import { searchDataRain } from "./services/buienradar.js";
 
-async function run(){
-    
+async function run() {
+  const dadosClima = await searchDataRain('52.09', '5.12');
+
+  const momentosChuva = dadosClima.forecasts.filter()
+  
+  console.log(dadosClima.forecasts);
 }
+run();
+
+
