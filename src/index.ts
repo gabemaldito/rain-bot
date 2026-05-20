291410125b1da9e1f707a41eb2e183f455167be1
gabemaldito <gabrielraphacosta@google.com>
2026-05-20 20:07:41 +0200

    implement rain moment filtering and alert messaging in run function

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,22 @@ import { searchDataRain } from "./services/buienradar.js";
 async function run() {
   const dadosClima = await searchDataRain('52.09', '5.12');
 
-  const momentosChuva = dadosClima.forecasts.filter()
-  
+    //filter and rain moments
+  const momentosChuva = dadosClima.forecasts.filter((item:any) => item.precipitation > 0);
+    
+    //check if the list have elements
+    if(momentosChuva.length > 0){
+        const firstMoment = momentosChuva[0];
+        const lastMoment = momentosChuva[momentosChuva.length - 1];
+
+        const startTime = firstMoment.datetime.split('T')[1].slice(0, 5);
+        const endTime = lastMoment.datetime.split('T')[1].slice(0, 5);
+        
+        console.log(`Alerta! Vai chover de ${startTime} até ${endTime}`);
+    }else{
+        console.log("Pode sair tranquilo, sem previsão de chuva para as próximas 2 horas!");
+    }
+
   console.log(dadosClima.forecasts);
 }
 run();
