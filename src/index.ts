0e77efd0f226a8eb1b09a1f5eccbac4258feedf8
gabemaldito <gabrielraphacosta@google.com>
2026-05-22 21:41:53 +0200

    feat: add express server and webhook for rain alerts
    - Integrated express framework to handle HTTP requests.
    - Created a POST endpoint "/webhook" to fetch rain data and log alerts.
    - Added @types/express as a dev dependency for TypeScript support.
    - Included express as a dependency in package.json.

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,39 @@
 import { searchDataRain } from "./services/buienradar.js";
+import express, { Request, Response } from "express";
 
-async function run() {
-  const dadosClima = await searchDataRain('52.09', '5.12');
+const app = express();
+const port = process.env.PORT || 3000;
 
-    //filter and rain moments
-  const momentosChuva = dadosClima.forecasts.filter((item:any) => item.precipitation > 0);
-    
-    //check if the list have elements
-    if(momentosChuva.length > 0){
-        const firstMoment = momentosChuva[0];
-        const lastMoment = momentosChuva[momentosChuva.length - 1];
+app.use(express.json());
 
-        const startTime = firstMoment.datetime.split('T')[1].slice(0, 5);
-        const endTime = lastMoment.datetime.split('T')[1].slice(0, 5);
-        
-        console.log(`Alerta! Vai chover de ${startTime} até ${endTime}`);
-    }else{
-        console.log("Pode sair tranquilo, sem previsão de chuva para as próximas 2 horas!");
-    }
+app.post("/webhook", async (req: Request, res: Response) => {
+  const dadosClima = await searchDataRain("52.09", "5.12");
+
+  //filter and rain moments
+  const momentosChuva = dadosClima.forecasts.filter(
+    (item: any) => item.precipitation > 0,
+  );
+
+  //check if the list have elements
+  if (momentosChuva.length > 0) {
+    const firstMoment = momentosChuva[0];
+    const lastMoment = momentosChuva[momentosChuva.length - 1];
+
+    const startTime = firstMoment.datetime.split("T")[1].slice(0, 5);
+    const endTime = lastMoment.datetime.split("T")[1].slice(0, 5);
+
+    console.log(`Alerta! Vai chover de ${startTime} até ${endTime}`);
+  } else {
+    console.log(
+      "Pode sair tranquilo, sem previsão de chuva para as próximas 2 horas!",
+    );
+  }
 
   console.log(dadosClima.forecasts);
-}
-run();
 
+  res.sendStatus(200);
+});
 
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
